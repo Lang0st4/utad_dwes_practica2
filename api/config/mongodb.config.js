610da6651131308225dcb140641aaaaf3997803f
Lang0st4 <96775390+Lang0st4@users.noreply.github.com>
2025-11-04 19:21:41 +0100

    MongoDB connection changes

--- api/config/mongodb.config.js
+++ api/config/mongodb.config.js
@@ -1,21 +1,21 @@
 //Import
 const mongoose = require("mongoose");
+const DB_URI = process.env.DB_URI;
 
 //MongoDB connnection
-const dbConnect = () => {
-    const DB_URI = process.env.DB_URI;
-    mongoose.set('strictQuery', false);
-    mongoose.connect(DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, (err, res) => {
-        if (!err) {
-            console.log("MongoDB connected successfully...")
-        }
-        else {
-            console.err("Error while connecting to MongoDB")
-        }
-    })
+const dbConnect = async () => {
+    console.log("Initializing MongoDB connection...")
+    try {
+        await mongoose.connect(DB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("MongoDB connected successfully");
+    }
+    catch(err) {
+        console.err("MongoDB connection failed :\n" + err);
+        process.exit(1);
+    }
 };
 
 //Export
